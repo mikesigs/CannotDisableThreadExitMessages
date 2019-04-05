b11e054cd087c39b0c61e13119b313cccb1f9ea6
Mike Sigsworth <mikesigsworth@gmail.com>
2019-04-05 11:24:20 -0600

    Simple console app to show Thread Exit messages aren't turned off with Docker Compose.

---
+++ CannotDisableThreadExitMessages/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+namespace CannotDisableThreadExitMessages
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var t = new Thread(DoStuff);
+                t.Start();
+            }
+        }
+
+        private static void DoStuff()
+        {
+            Thread.Sleep(50);
+        }
+    }
+}
